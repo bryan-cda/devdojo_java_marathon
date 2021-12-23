51aa374938b1a53558e89c07e2c709878ec7e21e
Bryan Duarte <albuquerque.bry.n@gmail.com>
2021-12-23 02:11:16 -0300

    Introduce to functional programming using UnaryOperator.

---
+++ dio/src/br/com/functional/programming/UnaryOperatorUse.java
@@ -0,0 +1,16 @@
+package br.com.functional.programming;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorUse {
+    public static void main(String[] args) {
+        UnaryOperator<Integer> result = number -> multiply(number, 3);
+        System.out.println(result.apply(3));
+    }
+
+    public static Integer multiply(Integer number, Integer multiplyFor){
+        return number * multiplyFor;
+    }
+
+
+}
