b9107e59b2e1e87c55c4855426d442e508518504
Bryan Duarte <albuquerque.bry.n@gmail.com>
2022-01-03 01:33:58 -0300

    Update project.

--- dio/src/br/com/functional/programming/UnaryOperatorUse.java
+++ dio/src/br/com/functional/programming/UnaryOperatorUse.java
@@ -3,10 +3,6 @@ package br.com.functional.programming;
 import java.util.function.UnaryOperator;
 
 public class UnaryOperatorUse {
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         UnaryOperator<Integer> result = number -> multiply(number, 3);
         System.out.println(result.apply(3));
