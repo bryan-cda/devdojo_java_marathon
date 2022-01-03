acc9723fd4a48f1782895cf1f84bea367ee46946
Bryan Duarte <albuquerque.bry.n@gmail.com>
2022-01-03 01:17:36 -0300

    Apply memoization use of functional programming.

--- dio/src/br/com/functional/programming/UnaryOperatorUse.java
+++ dio/src/br/com/functional/programming/UnaryOperatorUse.java
@@ -3,6 +3,10 @@ package br.com.functional.programming;
 import java.util.function.UnaryOperator;
 
 public class UnaryOperatorUse {
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         UnaryOperator<Integer> result = number -> multiply(number, 3);
         System.out.println(result.apply(3));
