ad7af189ca556d6b76196bca5919e359f87bfdf9
Bryan Duarte <albuquerque.bry.n@gmail.com>
2022-01-03 22:48:39 -0300

    First use of function using Java 8 API.

--- dio/src/br/com/functional/programming/HighOrderFunctionUse.java
+++ dio/src/br/com/functional/programming/HighOrderFunctionUse.java
@@ -2,9 +2,15 @@ package br.com.functional.programming;
 
 public class HighOrderFunctionUse {
     public static void main(String[] args) {
-        Calculator calc = (a,b) -> a + b;
+        Calculator sum = (a,b) -> a + b;
+        Calculator multiplication = (a,b) -> a * b;
+        Calculator division = (a,b) -> a / b;
+        Calculator subtraction = (a,b) -> a - b;
 
-        System.out.println(calculatorHighOrderFunction(calc, 12, 7));
+        System.out.println(calculatorHighOrderFunction(sum, 12, 7));
+        System.out.println(calculatorHighOrderFunction(multiplication, 12, 7));
+        System.out.println(calculatorHighOrderFunction(division, 12, 7));
+        System.out.println(calculatorHighOrderFunction(subtraction, 12, 7));
     }
 
     public static int calculatorHighOrderFunction(Calculator calc, int a, int b){
