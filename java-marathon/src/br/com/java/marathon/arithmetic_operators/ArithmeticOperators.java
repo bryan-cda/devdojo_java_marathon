8c05689b0a543be609bc9d42b1b9c0a41abbaea3
Bryan Duarte <bryan.duarte@ext.valid.com>
2022-03-07 18:17:37 -0300

    (study) Use relation operators to compare value and return values by condition.

--- java-marathon/src/br/com/java/marathon/arithmetic_operators/ArithmeticOperators.java
+++ java-marathon/src/br/com/java/marathon/arithmetic_operators/ArithmeticOperators.java
@@ -1,9 +1,5 @@
 package br.com.java.marathon.arithmetic_operators;
 
-import java.util.Scanner;
-
-import static java.lang.String.format;
-
 public class ArithmeticOperators {
     public static void main(String[] args) {
         //Arithmetic operators
@@ -18,20 +14,5 @@ public class ArithmeticOperators {
         //Cast to double
         double result = (double) num2 / num1;
         System.out.println(result);
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is the selected number?");
-        Integer selectedNumber = scanner.nextInt();
-
-
-        //Rest and relational operators use
-        System.out.println(selectedNumber % 3 > 1
-                && selectedNumber / 2 > 1
-                || selectedNumber - num1 > 1
-                ? format("Is rest %s major than 1", selectedNumber % 3)
-                : format("Is rest %s lower or equal to 1", (selectedNumber % 3))
-        );
-
-
     }
 }
