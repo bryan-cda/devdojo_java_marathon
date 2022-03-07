9dcf6582ae1302bb8b45743d8e1e710df45cbbff
Bryan Duarte <bryan.duarte@ext.valid.com>
2022-03-07 13:00:22 -0300

    (study) Practiced using relational,logical and arithmetic operators.

---
+++ java-marathon/src/br/com/java/marathon/arithmetic_operators/ArithmeticOperators.java
@@ -0,0 +1,25 @@
+package br.com.java.marathon.arithmetic_operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        //Arithmetic operators
+        int num1 = 20;
+        int num2 = 10;
+
+        System.out.println("Multiplication: " + num1 * num2);
+        System.out.println("Sum:" + num1 + num2);
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Division: " + (num1 - num2) + "\n");
+
+        //Cast to double
+        double result = (double) num2 / num1;
+        System.out.println(result);
+
+
+        //Rest and relational operators use
+        System.out.println(20 % 3 > 1
+                ? String.format("Is %s major than 1", 20 % 3)
+                : String.format("Is %s lower or equal than 1", 20 % 3)
+        );
+    }
+}
---
+++ java-marathon/src/br/com/java/marathon/relational_operators/RelationalOperators.java
@@ -0,0 +1,7 @@
+package br.com.java.marathon.relational_operators;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+
+    }
+}
