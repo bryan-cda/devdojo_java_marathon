340016229a14dcb9d9df9e9b9204776ec501bf3b
Bryan Duarte <bryan.duarte@ext.valid.com>
2022-03-09 15:25:36 -0300

    (study) Studying attributions values to variables.

---
+++ java-marathon/src/br/com/java/marathon/attributions/Attributions.java
@@ -0,0 +1,23 @@
+package br.com.java.marathon.attributions;
+
+public class Attributions {
+    public static void main(String[] args) {
+        int value = 10;
+
+        System.out.println(value);
+        value += 1;
+        System.out.println(value);
+        value -= 1;
+        System.out.println(value);
+        value *= 2;
+        System.out.println(value);
+        value /= 2;
+        System.out.println(value);
+        value %= 2;
+        System.out.println(value);
+        value++;
+        System.out.println(value);
+        value--;
+        System.out.println(value);
+    }
+}
