5a7c2b5b57f441bc54711d90a0064dd7ab96381e
Bryan Duarte <albuquerque.bry.n@gmail.com>
2022-12-27 13:36:52 -0300

    40 - Orientação Objetos - Introdução classes pt 02.

--- java-marathon/src/br/com/java/marathon/oo/OOTest.java
+++ java-marathon/src/br/com/java/marathon/oo/OOTest.java
@@ -4,13 +4,22 @@ import br.com.java.marathon.oo.domain.Student;
 
 public class OOTest {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.name = "Foo";
-        student.gender = 'M';
-        student.age = 30;
+        Student student1 = new Student();
+        student1.name = "Foo";
+        student1.gender = 'M';
+        student1.age = 30;
 
-        System.out.println(student.name);
-        System.out.println(student.gender);
-        System.out.println(student.age);
+        Student student2 = new Student();
+        student2.name = "Bar";
+        student2.gender = 'F';
+        student2.age = 31;
+
+        System.out.println(student1.name);
+        System.out.println(student1.gender);
+        System.out.println(student1.age);
+
+        System.out.println(student2.name);
+        System.out.println(student2.gender);
+        System.out.println(student2.age);
     }
 }
