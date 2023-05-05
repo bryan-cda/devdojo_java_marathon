9603ed7922e18a848d105ca06ce644509af82fe0
Bryan Duarte <56884795+bryan-cda@users.noreply.github.com>
2023-05-05 15:41:51 -0300

    Delete java-marathon/src/br/com/java/marathon directory

--- java-marathon/src/br/com/java/marathon/oo/domain/StudentPrinter.java
+++
@@ -1,9 +0,0 @@
-package br.com.java.marathon.oo.domain;
-
-public class StudentPrinter {
-    public void printStudentData(Student student){
-        System.out.println(student.getName());
-        System.out.println(student.getAge());
-        System.out.println(student.getGender());
-    }
-}
